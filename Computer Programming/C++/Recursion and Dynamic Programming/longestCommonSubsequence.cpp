3eec48edef574935aaef336c9a6ac94b3edef80c
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-08-01 02:44:43 +0530

    Update longestCommonSubsequence.cpp

--- Computer Programming/C++/Recursion and Dynamic Programming/longestCommonSubsequence.cpp
+++ Computer Programming/C++/Recursion and Dynamic Programming/longestCommonSubsequence.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) 
     {
-        int index1 = text1.size() -1;
-        int index2 = text2.size() -1;  
-
-        vector<vector<int>> vis(index1+1,vector<int>(index2+1,-1));
-                        
-        int ans = maxsize(index1,index2,text1,text2,vis);
+        int text1size = text1.size();
+        int text2size = text2.size();
+        vector<vector<int>> dp(text1size,vector<int>(text2size,-1));
+        int ans = function(text1,text1size - 1,text2,text2size - 1,dp);   
         return ans;
     }
-
-    int maxsize(int index1,int index2,string text1, string text2,vector<vector<int>> &vis)
-    {
-        if(index1 < 0 || index2 < 0)
+        int function(string &text1, int index1, string &text2, int index2, vector<vector<int>>& dp)
         {
-            return 0;
-        }
-
-        if(vis[index1][index2]!=-1)
-        {
-            return vis[index1][index2];
-        }
+            if(index1<0 || index2<0)
+            {
+                return 0;
+            }
+            if(dp[index1][index2] != -1)
+            {
+                return dp[index1][index2];
+            }
 
         if(text1[index1] == text2[index2])
-        {            
-            return vis[index1][index2] = 1 + maxsize(index1-1,index2-1,text1,text2,vis);
+        {
+            return dp[index1][index2] = 1 + function(text1,index1-1,text2,index2-1,dp);
         }
         else
         {
-            return vis[index1][index2] = 0 + max(maxsize(index1-1,index2,text1,text2,vis),maxsize(index1,index2-1,text1,text2,vis));
+            return dp[index1][index2] = max(function(text1,index1-1,text2,index2,dp), function(text1,index1,text2,index2-1,dp));
         }
-    }
+
+        }
 };
