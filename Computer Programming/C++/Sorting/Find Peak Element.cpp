4d716a6ca3170629d0fa4a11ab56dfbadf893ae8
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-08-17 03:50:20 +0530

    Create Find Peak Element.cpp

---
+++ Computer Programming/C++/Sorting/Find Peak Element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        int mid;
+        while(left<right)
+        {
+            mid = (left + right)/2;
+            if(nums[mid] > nums[mid+1])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid+1;
+            }
+        }
+        return left;
+            
+    }
+};
