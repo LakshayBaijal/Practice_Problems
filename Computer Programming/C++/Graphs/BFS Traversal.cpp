00811cdea225f47fab2fae103eb6b7bda6c030ca
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-08-28 02:33:24 +0530

    Update BFS Traversal.cpp

--- Computer Programming/C++/Graphs/BFS Traversal.cpp
+++ Computer Programming/C++/Graphs/BFS Traversal.cpp
@@ -1,43 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-  int n, m;
-  cout << "Total Nodes";
-  cin >> n;
-
-  cout << "Total Edges";
-  cin >> m;
-
-  vector<int> adjlist[n];
-
-  for (int i = 0; i < m; i++) {
-    int u, v;
-    cin >> u >> v;
-    adjlist[u].push_back(v);
-    adjlist[v].push_back(u);
-  }
-
-  int vis[n];
-  vis[n] = {0};
-  vis[0] = 1;
-  queue<int> q;
-
-  q.push(0);
-  vector<int> bfs;
-  while (q.empty() == false) {
-    int node = q.front();
-    q.pop();
-    bfs.push_back(node);
-    for (auto it : adjlist[node]) {
-      if (vis[it] == 0) {
-        vis[it] = 1;
-        q.push(it);
-      }
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) 
+    {
+        // Code here
+        int v = adj.size();
+        queue<int>q;
+        vector<int>vis(v,0);
+        
+        q.push(0);
+        vector<int>bfs;
+        vis[0] = 1;
+        while(q.empty() == false)
+        {
+            int n = q.front();
+            q.pop();
+            
+            bfs.push_back(n);
+            
+            for(auto it:adj[n])
+            {
+                if(vis[it] != 1)
+                {
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        return bfs;
+        
+        
+        
     }
-  }
-
-  for (auto it : bfs) {
-    cout << it << " ";
-  }
-}
+};
