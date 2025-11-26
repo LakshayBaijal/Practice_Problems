5999467bcfc9ade8940061e0c3db867be0580b19
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-11-27 04:50:42 +0530

    Create Bellman Ford.cpp

---
+++ Computer Programming/C++/Graphs/Weighted Graph/Bellman Ford.cpp
@@ -0,0 +1,39 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) 
+    {
+        // Code here
+        vector<vector<pair<int,int>>> adjlist(V);
+        vector<int>dist(V,INT_MAX);
+        dist[src] = 0;
+        for(int i = 0;i<V;i++)
+        {
+            for(auto &it : edges)
+            {
+                int u = it[0];
+                int v = it[1];
+                int w = it[2];
+                
+                if(dist[u] != INT_MAX && dist[v] > w + dist[u])
+                {
+                    dist[v] = w + dist[u];
+                }
+            }
+        }
+
+            for(auto &it : edges)
+            {
+                int u = it[0];
+                int v = it[1];
+                int w = it[2];
+                
+                if(dist[u] != INT_MAX && dist[v] > w + dist[u])
+                {
+                    return {-1};
+                }
+            }
+        return {1};
+    }
+};
