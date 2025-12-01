0b058e66e832d2103f637bcdf2e05887d3f5f5d0
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-12-02 03:36:38 +0530

    Create 1.py

---
+++ Computer Programming/Python/Basics/1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([[1,2,3],[4,5,6]])
+
+print(a)
+print(b)
+
+print(a + b)
+
+print(a[1:2])
+
+c = a[1]
+
+print(c)
+
+
+d = np.array([3,2,1])
+
+
+print(np.dot(a,d))
