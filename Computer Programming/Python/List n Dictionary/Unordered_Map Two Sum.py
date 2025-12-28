06517cd8a6b82244edd6349653364ba92ea162cf
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-12-28 06:00:39 +0530

    Create Unordered_Map Two Sum.py

---
+++ Computer Programming/Python/List n Dictionary/Unordered_Map Two Sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mp = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in mp:
+                return [mp[diff],i]
+            mp[nums[i]] = i
+        
+        return {}
