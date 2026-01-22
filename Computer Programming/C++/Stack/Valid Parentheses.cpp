40d0b9f4a1b835ac9e9c706169b752f1241cbeb7
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2026-01-23 01:32:58 +0530

    Create Valid Parentheses.cpp

---
+++ Computer Programming/C++/Stack/Valid Parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+
+        for (auto it : s)
+        {
+            if (it == '(' || it == '[' || it == '{')
+            {
+                st.push(it);
+            }
+            else if (it == ')')
+            {
+                if (st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+            else if (it == '}')
+            {
+                if (st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+            }
+            else if (it == ']')
+            {
+                if (st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
