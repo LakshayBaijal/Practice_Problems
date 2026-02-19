df8ada73d951fe181108e7d16e2c4dc56c14026f
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2026-02-20 03:52:32 +0530

    Create Longest Arithmetic Subsequence.cpp

---
+++ Computer Programming/C++/Difficult/Longest Arithmetic Subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) 
+    {
+
+        int n = nums.size();
+
+        if(n == 2)
+        {
+            return 2;
+        }
+
+        vector<unordered_map<int,int>>dp(n);
+        int ans = 0;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = i + 1;j<n;j++)
+            {
+                int diff = nums[j] - nums[i];
+                if(dp[i].count(diff))
+                {
+                    dp[j][diff] = dp[i][diff] + 1;
+                }
+                else
+                {
+                    dp[j][diff] = 2;
+                }
+
+                ans = max(ans,dp[j][diff]);
+            }
+        }
+
+        return ans;
+    }
+};
