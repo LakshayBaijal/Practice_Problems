45e8b93982eb1caf6b1dd92389effa72614ceae8
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2026-03-04 04:21:12 +0530

    Create 0 - 1 Knapsack Problem.cpp

---
+++ Computer Programming/C++/Difficult/0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) 
+    {
+        // code here
+        int n = val.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(W+1,0));
+        
+        for(int i = 1; i <= n;i++)
+        {
+            for(int j = 0;j <= W; j++)
+            {
+                dp[i][j] = dp[i-1][j];
+                if(wt[i-1] <= j)
+                {
+                    dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]], dp[i][j]);
+                }
+                
+            }
+        }
+        return dp[n][W];
+        
+    }
+};
