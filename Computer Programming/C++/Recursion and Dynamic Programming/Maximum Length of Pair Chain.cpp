78ef21c18322dd0470e08b697213446cb48111de
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2026-03-05 05:27:08 +0530

    Create Maximum Length of Pair Chain.cpp

---
+++ Computer Programming/C++/Recursion and Dynamic Programming/Maximum Length of Pair Chain.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) 
+    {
+        int n = pairs.size();
+        vector<int>dp(n,1);
+        sort(pairs.begin(), pairs.end());
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                if(pairs[j][1] < pairs[i][0])
+                {
+                    dp[i] = max(dp[i],dp[j] + 1);
+                }
+            }
+        }
+        
+        return *max_element(dp.begin(),dp.end());
+    }
+};
